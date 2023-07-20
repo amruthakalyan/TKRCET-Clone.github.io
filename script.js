57be7e23c037a82e871bb23dad7ff08555b2e541
amruthakalyan <116357692+amruthakalyan@users.noreply.github.com>
2023-07-20 12:44:10 +0530

    TKRCET Website CLONE

---
+++ script.js
@@ -0,0 +1,6 @@
+alert("Welcome to TKRCET :)");
+let sublist=document.getElementsByClassName("sublist");
+let hamberger=document.getElementById('hamberger');
+hamberger.addEventListener('click',()=>{
+sublist.style.display="block";
+})
