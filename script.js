6ef5a7e604055965c44090c51004db5f80dd77c3
amruthakalyan <116357692+amruthakalyan@users.noreply.github.com>
2023-07-21 23:35:55 +0530

    clone

--- script.js
+++ script.js
@@ -21,3 +21,23 @@ function slideshow(){
         setTimeout(slideshow,3000);
 }
 slideshow();
+
+//click function
+let img=document.getElementsByClassName('img1');
+let info=document.getElementsByClassName('info');
+console.log(info);
+console.log(img);
+for(let i=0;i<img.lenght,i<info.length;i++)
+   {
+      img[i].addEventListener('click',()=>{
+        info[i].style.display='block';
+        info[i].style.backgroundColor="gainsboro";
+      })
+   }
+  let marquee=document.getElementsByClassName('marquee');
+function  Stop(x){
+    x.stop();
+}
+function start(x){
+    x.start();
+}
