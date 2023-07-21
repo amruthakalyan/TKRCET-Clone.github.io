787c5ffaab496500ea3cfb1cd7ecea6cddf36b38
amruthakalyan <116357692+amruthakalyan@users.noreply.github.com>
2023-07-21 17:55:30 +0530

    clone

--- script.js
+++ script.js
@@ -3,21 +3,21 @@ alert("Welcome to TKRCET :)");
 //slideshow using javascript
 
 let slideindex=0;
-slideshow();
 function slideshow(){
     let i;
-    let slides = document.querySelectorAll('.slides');
+    let slides = document.getElementsByClassName('slides');
     for( i=0;i<slides.length;i++){
         slides[i].style.display='none';
     }
-    slideindex++;
-    if(slideindex === slides.lenght){
-        slideindex = 0;
-    }
-    slides[slideindex-1].style.display='block';
-    setTimeout(slideshow,2000);
+     slideindex++;
+     if(slideindex === slides.lenght){
+         slideindex =0;
+     }
+     if(slideindex > slides.length-1)
+     {
+        slideindex=1;
+     }
+         slides[slideindex-1].style.display='block';
+        setTimeout(slideshow,3000);
 }
-let notification=document.getElementsByTagName('span');
-    this.addEventListener('click',()=>{
-    this.stop();
-})
+slideshow();
